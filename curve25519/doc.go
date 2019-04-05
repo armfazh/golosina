0a7313c4957781862c93c28bb3147a52ffd9425f
armfazh <armfazh@gmail.com>
2019-04-05 14:05:44 -0700

    Adding curve25519 amd64 implementation.

---
+++ curve25519/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package curve25519 provides an implementation of scalar multiplication on
+// the elliptic curve known as curve25519. See https://cr.yp.to/ecdh.html
+package curve25519
